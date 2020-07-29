9e548c45cf04d920a98eb184d6d9de9f6afe21f9
Alex <alexandergeorgesmith@gmail.com>
2020-07-30 00:58:07 +0100

    Add Google Analytics scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,16 @@
         <meta property="og:type" content="website" />
 
         <meta name="robots" content="index, follow" />
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-97464582-3"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-97464582-3');
+        </script>
     </head>
     <body>
 
