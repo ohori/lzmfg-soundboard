a2af690974c9a70e9752a5db66fb34fdca543862
Alex <alexandergeorgesmith@gmail.com>
2020-07-30 00:39:50 +0100

    Update footer text

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,8 +30,8 @@
         <section class="hero is-light">
             <div class="hero-body">
                 <div class="container has-text-centered">
-                    <p>This is a bespoke, fan made project with no intention of producing revenue.</p>
-                    <p>I do not own any of the sounds used. All sounds extracted from <a href="https://www.youtube.com/user/LZBMX" target="_blank">AdamLZ</a> YouTube channel.</p>
+                    <p>This is a bespoke, fan made project that is still work in progress.</p>
+                    <p>All sounds extracted from <a href="https://www.youtube.com/user/LZBMX" target="_blank">AdamLZ</a> YouTube channel.</p>
                     <br />
                     <p>Check out the code on <a href="https://github.com/ohori/lzmfg-soundboard" target="_blank">GitHub</a></p>
                     <p class="subtitle">
